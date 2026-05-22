8942009278900e05950d40582fa5cbc0d90f51e7
Cristian Acevedo <acecristian35@gmail.com>
2026-05-22 11:33:24 -0400

    p3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -29,3 +29,13 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher('John', 'Doe'));
